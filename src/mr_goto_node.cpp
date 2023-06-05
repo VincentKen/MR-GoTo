136280718c630e05df5e24599f4c9a083d7ffab9
Benjamin-Ga <benjamin.gallauner@gmail.com>
2023-06-05 11:32:45 +0200

    implemented ground_truth subscriber

--- src/mr_goto_node.cpp
+++ src/mr_goto_node.cpp
@@ -1,6 +1,11 @@
 #include "mr_goto/mr_goto_node.hpp"
 
 GoToNode::GoToNode(rclcpp::NodeOptions options) : Node("goto", options) {
+    sub_ground_truth_ = create_subscription<nav_msgs::msg::Odometry>(
+        "ground_truth",
+        10, std::bind(&GoToNode::callback_ground_truth, this, std::placeholders::_1));
+    RCLCPP_INFO(this->get_logger(), "subscribed to ground_truth");
+
     timer_ = this->create_wall_timer(
         std::chrono::milliseconds(5000),
         std::bind(&GoToNode::timer_callback, this)
@@ -9,4 +14,10 @@ GoToNode::GoToNode(rclcpp::NodeOptions options) : Node("goto", options) {
 
 void GoToNode::timer_callback() {
     RCLCPP_INFO(this->get_logger(), "MR GOTO Timer Callback");
+}
+
+void GoToNode::callback_ground_truth(const nav_msgs::msg::Odometry::SharedPtr msg)
+{
+    this->ground_truth_ = msg;
+    RCLCPP_INFO(this->get_logger(), std::to_string(msg->pose.pose.position.x).data());
 }
